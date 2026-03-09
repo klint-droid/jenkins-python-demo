960ed79df0398a2e76939573cf550b6a998f6744
Klint Ruales <klintruales11@gmail.com>
2026-03-09 22:23:11 +0800

    add

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+@app.route("/add")
+def add():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(a + b)
 
 if __name__ == "__main__":
     app.run(port=5000)
--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 from app import app
 
-def test_home():
+def test_add():
     client = app.test_client()
-    response = client.get("/")
-    assert response.status_code == 200
+    response = client.get("/add?a=2&b=6")
+    assert response.data == b"8"
